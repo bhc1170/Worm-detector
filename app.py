0b2852ec401b86324847913b4525d3064a580586
Nigel Lawrence <beyondhorizonllc19@gmail.com>
2024-08-08 13:37:11 -0400

    Add Flask app script to bind to PORT

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ port = int(os.environ.get('PORT', 5000))
 
 @app.route('/')
 def hello():
-    return "Hello, World!"
+    return 'Hello, World!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
